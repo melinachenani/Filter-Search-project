20ef623f17a19e1f9ee3a5a5a9d1e54016abeb95
melinachenani <melina.ch98@gmail.com>
2023-07-03 16:15:57 +0330

    updating index.js

--- index.js
+++ index.js
@@ -41,3 +41,102 @@ function closeCart (){
     cartCenter.style.opacity = "0";
     overLay.style.display = "none";
 }
+
+const searchInput = document.querySelector("#search");
+const productsDOM = document.querySelector(".products");
+const btns = document.querySelectorAll(".btn-primary");
+const storeProducts = document.querySelectorAll(".product-container");
+
+let allProductsdata = [];
+const filters = {
+    searchItems: "",
+};
+
+// http://localhost:3000/items
+document.addEventListener("DOMContentLoaded", () =>{
+    // console.log("loaded...");
+    // axios
+    // .get("http://localhost:3000/items")
+    // .then((res)=>console.log(res.data))
+    // .catch((err)=>console.log(err));
+    axios
+    .get("http://localhost:3000/items")
+    .then((res)=>{
+        allProductsdata = res.data;
+        //render products on DOM
+        renderProducts(res.data, filters);
+    })
+    .catch((err)=>console.log(err));
+});
+
+function renderProducts (_products, _filters) {
+    const filteredProducts = _products.filter((p) => {
+    return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
+    });
+    productsDOM.innerHTML = "";
+    // console.log(filteredProducts);
+    // render to DOM
+    filteredProducts.forEach( (item) => {
+        //create
+        //content
+        //append to products
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("single-product");
+    productDiv.innerHTML =`
+    <div class="product">
+        <div class="image-container">
+            <div class="image">
+                <img src=${item.image} alt="" class="product-img">
+            </div>
+        </div>
+        <div class="product-desc">
+            <h5 class="product-title">${item.title}</h5>
+            <span class="product-price">${item.price}</span>                    
+        </div>
+    </div>`;
+    // append
+    productsDOM.appendChild(productDiv);
+    });
+}
+
+searchInput.addEventListener("input", (e)=> {
+    // console.log(e.target.value);
+    filters.searchItems = e.target.value;
+    renderProducts(allProductsdata, filters);
+});
+
+//filter based on groups
+function renderBasedOnClass (_products, _filters) {
+    const filteredProducts = _products.filter((p) => {
+    return p.class.toLowerCase().includes(_filters.searchItems.toLowerCase());
+    });
+    productsDOM.innerHTML = "";
+    filteredProducts.forEach( (item) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("single-product");
+    productDiv.innerHTML =`
+    <div class="product">
+        <div class="image-container">
+            <div class="image">
+                <img src=${item.image} alt="" class="product-img">
+            </div>
+        </div>
+        <div class="product-desc">
+            <h5 class="product-title">${item.title}</h5>
+            <span class="product-price">${item.price}</span>                    
+        </div>
+    </div>`;
+
+    productsDOM.appendChild(productDiv);
+    });
+}
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const filter = e.target.dataset.filter;
+    //   e.target.classList.add("active");
+    //   console.log(e.target.innerText);
+      filters.searchItems = filter;
+      renderBasedOnClass(allProductsdata, filters);
+    });
+});
