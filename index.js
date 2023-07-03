e78c92400ee1765831582e8e2b819abda74bf4aa
melinachenani <melina.ch98@gmail.com>
2023-07-03 16:12:13 +0330

    updating index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 //selectors
-const loginBox = document.querySelector(".login-box");
-const backDrop = document.querySelector(".backdrop");
-const showLoginBox = document.querySelector(".user");
-const closeLoginBox = document.querySelector(".submit");
 const toggler = document.querySelector(".nav__toggler");
 const navbar = document.querySelector(".nav");
+const loginBox = document.querySelector(".login-box");
+const showLoginBox = document.querySelector(".user");
+const closeLoginBox = document.querySelector(".submit");
+const backDrop = document.querySelector(".backdrop");
+const cartCenter = document.querySelector(".cart-center");
+const showCartModal = document.querySelector(".cart");
+const closeCartModal = document.querySelector(".submit");
+const overLay = document.querySelector(".overlay");
 
 //event listener
+toggler.addEventListener("click", hamburgerMenu);
 showLoginBox.addEventListener("click", showModal);
 closeLoginBox.addEventListener("click", closeModal);
 backDrop.addEventListener("click", closeModal);
-toggler.addEventListener("click", hamburgerMenu);
+showCartModal.addEventListener("click", showCart);
+closeCartModal.addEventListener("click", closeCart);
+overLay.addEventListener("click", closeCart);
+
 
 //Functions
 function hamburgerMenu(){
@@ -24,4 +32,12 @@ function showModal () {
 function closeModal (){
     loginBox.style.opacity = "0";
     backDrop.style.display = "none";
+}
+function showCart () {
+    cartCenter.style.opacity = "1";
+    overLay.style.display = "block";
+}
+function closeCart (){
+    cartCenter.style.opacity = "0";
+    overLay.style.display = "none";
 }
